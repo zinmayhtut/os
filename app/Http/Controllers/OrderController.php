149d30084912e8d8df27f2ccc7a3073eccf72809
zinmayhtut <suhtetwai215@gmail.com>
2020-08-17 17:17:36 +0630

    checkout error3

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Order;
 use App\User;
-use Auth;
+use Illuminate\Support\Facades\Auth;
 
 class OrderController extends Controller
 {
